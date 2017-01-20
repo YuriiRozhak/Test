00eae8b59d273c8f2100fb277226a283ab506088
YuriiRozhak <manrocker1994@gmail.com>
2017-01-20 13:45:01 +0200

    Implements basic methods

--- hiberlibrary/src/main/java/com/softserve/lv219/hiberlibrary/dao/GenericDAOImpl.java
+++ hiberlibrary/src/main/java/com/softserve/lv219/hiberlibrary/dao/GenericDAOImpl.java
@@ -1,8 +1,15 @@
 package com.softserve.lv219.hiberlibrary.dao;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
-public class GenericDAOImpl<E,T> implements GenericDAO<E,T> {
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+
+public class GenericDAOImpl<E,T extends Serializable> implements GenericDAO<E,T> {
 	
     private Class<E> genericClass;
 
@@ -12,28 +19,80 @@ public class GenericDAOImpl<E,T> implements GenericDAO<E,T> {
 
 
 	public void addElement(E element) {
-		// TODO Auto-generated method stub
+		Session session = null;
+        try {
+           session =	HibernateUtil.getSessionFactory().openSession();
+           Transaction transaction = session.beginTransaction();
+           session.save(element);
+           transaction.commit();
+        } finally {
+            if ((session != null) && (session.isOpen())) {
+                session.close();
+            }
+        }
+
 		
 	}
 
 	public void updateElement(E element) {
-		// TODO Auto-generated method stub
-		
+		Session session = null;
+        try {
+           session =HibernateUtil.getSessionFactory().openSession();
+           Transaction transaction = session.beginTransaction();
+           session.update(element);
+           transaction.commit();
+        } finally {
+            if ((session != null) && (session.isOpen())) {
+                session.close();
+            }
+        }
+
 	}
 
 	public E getElementByID(T elementId) {
-		// TODO Auto-generated method stub
-		return null;
+		Session session = null;
+        E element = null;
+        try {
+           session = HibernateUtil.getSessionFactory().openSession();
+           // element = (E) session.load(elementClass, elementId);
+           element = (E) session.get(genericClass, elementId);
+        } finally {
+            if ((session != null) && (session.isOpen())) {
+                session.close();
+        }    }
+        return element;
+
 	}
 
 	public List<E> getAllElements() {
-		// TODO Auto-generated method stub
-		return null;
+		Session session = null;
+	       List<E> elements = new ArrayList<E>();
+	       try {
+	          session = HibernateUtil.getSessionFactory().openSession();
+	          CriteriaBuilder builder = session.getCriteriaBuilder();
+	          CriteriaQuery<E> criteria = builder.createQuery(genericClass);
+	          criteria.from(genericClass);
+	          elements = session.createQuery(criteria).getResultList();
+	       } finally {
+	           if ((session != null) && (session.isOpen())) {
+	                session.close();
+	        }   }
+	        return elements;
+
 	}
 
 	public void deleteElement(E element) {
-		// TODO Auto-generated method stub
-		
+		Session session = null;
+	      try {
+	          session = HibernateUtil.getSessionFactory().openSession();
+	          Transaction transaction = session.beginTransaction();
+	          session.delete(element);
+	          transaction.commit();
+	      } finally {
+	           if ((session != null) && (session.isOpen())) {
+	                session.close();
+	       }    }
+
 	}
 
 }
