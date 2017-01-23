ed17ee98faeac328d4ceb2af6921666f2302d924
YuriiRozhak <manrocker1994@gmail.com>
2017-01-23 20:57:01 +0200

    Added BookService with basic methods

---
+++ hiberlibrary/src/main/java/com/softserve/lv219/hiberlibrary/services/BookService.java
@@ -0,0 +1,29 @@
+package com.softserve.lv219.hiberlibrary.services;
+
+import java.util.List;
+
+import com.softserve.lv219.hiberlibrary.dao.DAOFactory;
+import com.softserve.lv219.hiberlibrary.entity.Book;
+
+public class BookService {
+
+	public void addBook(Book book){
+		DAOFactory.getInstance().getBookDAO().addElement(book);
+	}
+	
+	public void updateBook(Book book){
+		DAOFactory.getInstance().getBookDAO().updateElement(book);
+	}
+	
+	public Book getBookById(Integer id){
+		return DAOFactory.getInstance().getBookDAO().getElementByID(id);
+	}
+	
+	public void deleteBook(Book book){
+		DAOFactory.getInstance().getBookDAO().deleteElement(book);
+	}
+	
+	public List<Book> getAllBooks() {
+		return DAOFactory.getInstance().getBookDAO().getAllElements();
+	}
+}
